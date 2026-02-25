104325c709a977bd68b65208637e773cb3329fa1
Eason0628 <zhaowintoo@gmail.com>
2026-02-25 19:07:41 +0800

    9.2 About BaseChatMemory Langchin记忆组件基类BaseChatMemory解析

---
+++ study/9-Memory组件运行流程及不同记忆分类/1.BaseChatMemory运行流程解析.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2026-02-24 17:12
+# @Author  : zhaowintoo@gmail.com
+# @File    : BaseChatMemory运行流程解析.py
+from langchain.memory import BaseChatMemory
+
+memory = BaseChatMemory(
+    input_key="query",
+    output_key="output",
+    return_messages=True,
+    # chat_history 假设
+)
+
+# 1. 加载记忆变量
+memory_variable = memory.load_memory_variables({})
+# 2. 调用模型
+# content = chain.invoke({"query": "你好，我是慕小课你是谁", "chat_history": memory_variable.get("chat_history")})
+# 3. 保存上下文
+# memory.save_context({"query": "你好，我是慕小课你是谁"}, {"output": "你好，我是ChatGPT,有什么可以帮到您的"})
+memory_variable = memory.load_memory_variables({})
+# content = chain.invoke({"query": "你好，我是慕小课你是谁", "chat_history": memory_variable.get("chat_history")})
