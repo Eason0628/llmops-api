bf596125efdf0f39754eba64e8a4eee941b90794
Eason0628 <zhaowintoo@gmail.com>
2026-03-07 15:14:55 +0800

    23.Usage of Document component and document loader component Document 组件与文档加载器组件的使用

---
+++ study/28-Document 组件与文档加载器组件的使用/1.Document与TextLoader.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from langchain_community.document_loaders import TextLoader
+
+# 1.构建加载器
+loader = TextLoader("./电商产品数据.txt", encoding="utf-8")
+
+# 2.加载数据
+documents = loader.load()
+
+print(documents)
+print(len(documents))
+print(documents[0].metadata)
